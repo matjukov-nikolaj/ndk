fa62b3243da6ac82bb7322faddd25fc424ee8f6e
matjukov-nikolaj <blatnoi.kolyan@yandex.ru>
2019-05-28 00:10:07 +0300

    added new backend component fixed sequence handler

--- SequenceHandler/SequenceHandler/Program.cs
+++ SequenceHandler/SequenceHandler/Program.cs
@@ -10,7 +10,6 @@ namespace SequenceHandler
     class Program
     {
         private static Dictionary<string, string> properties = Configuration.GetParameters();
-
         static void Main(string[] args)
         {
             Console.WriteLine("Sequence handler is running.");
@@ -27,7 +26,7 @@ namespace SequenceHandler
                         IDatabase redisDb = redisConnection.GetDatabase(dbNumber);
                         string value = redisDb.StringGet(id);
                         Console.WriteLine("Event: " + id + "-" + value);
-                        HandleSequence(id, value, redisConnection);
+                        HandleSequence(value, redisConnection);
                     }
                 });
                 Console.ReadKey();
@@ -38,37 +37,37 @@ namespace SequenceHandler
             }
         }
 
-        private static void HandleSequence(String id, string value, ConnectionMultiplexer redisConnection)
+        private static void HandleSequence(string value, ConnectionMultiplexer redisConnection)
         {
             try
             {
-                String nakedId = id.Replace("SEQUENCE_", "");
+                SequenceId sId = JsonConvert.DeserializeObject<SequenceId>(value);
                 
                 int grammarDbNumber = Convert.ToInt32(properties["GRAMMAR_DB"]);
                 IDatabase grammarDb = redisConnection.GetDatabase(grammarDbNumber);
 
-                string grammarStr = grammarDb.StringGet("GRAMMAR_" + nakedId);
+                string grammarStr = grammarDb.StringGet("GRAMMAR_" + sId.id);
                 Grammar grammar = JsonConvert.DeserializeObject<Grammar>(grammarStr);
                 
                 int newGrammarDbNumber = Convert.ToInt32(properties["NEW_GRAMMAR_DB"]);
                 IDatabase newGrammarDb = redisConnection.GetDatabase(newGrammarDbNumber);
 
-                string newGrammarStr = newGrammarDb.StringGet("NEW_GRAMMAR_" + nakedId);
+                string newGrammarStr = newGrammarDb.StringGet("NEW_GRAMMAR_" + sId.id);
                 NewGrammar newGrammar = JsonConvert.DeserializeObject<NewGrammar>(newGrammarStr);
                 
                 int mTableDbNumber = Convert.ToInt32(properties["TABLE_M_DB"]);
                 IDatabase mTableDb = redisConnection.GetDatabase(mTableDbNumber);
 
-                string mTableStr = mTableDb.StringGet("TABLE_M_" + nakedId);
+                string mTableStr = mTableDb.StringGet("TABLE_M_" + sId.id);
                 MTable mTable = JsonConvert.DeserializeObject<MTable>(mTableStr);
                 
                 SequenceHandler sequenceHandler = new SequenceHandler(grammar.startSymbol, newGrammar.terminals, newGrammar.noTerminals, mTable.mTable);
-                sequenceHandler.Process(value);
+                sequenceHandler.Process(sId.sequence);
                 
                 IDatabase redisDb = ConnectionMultiplexer.Connect(properties["REDIS_SERVER"])
                     .GetDatabase(Convert.ToInt32(properties["SEQUENCE_DB"]));
                 string json = JsonConvert.SerializeObject(sequenceHandler.GetSequence());
-                String newId = "SEQUENCE_RESULT_" + nakedId;
+                String newId = "SEQUENCE_RESULT_" + sId.id;
                 redisDb.StringSet(newId, json);
                 Console.WriteLine(newId + ": " + json + " - saved to redis SEQUENCE_DB");
             }
